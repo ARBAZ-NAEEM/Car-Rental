93041a79d785a01b8f1a7be818fda3d39e96ccf7
arbaz <arbaz.naeem@sybrid.com>
2024-01-10 20:40:57 +0500

    changes

--- src/components/Home/PickUpForm.jsx
+++ src/components/Home/PickUpForm.jsx
@@ -25,72 +25,70 @@ const PickUpForm = (props) => {
   };
 
   return (
-    <div className="extra-border">
-      <div className="section-form">
-        <Container>
-          <div className="form-section" style={{ boxShadow: boxShadow }}>
-            <Row style={{ alignItems: "flex-end" }}>
-              <Col lg="3" md="3" xs="12">
-                <FormGroupSelect
-                  label="Pick-up Location"
-                  onChange={handleChange}
-                  name="pickUpLocation"
-                  value={fields?.pickUpLocation}
-                  defaultName="Select Location"
-                />
-              </Col>
-              <Col lg="3" md="3" xs="12">
-                <div className="form-group">
-                  <label className="form-label">Pick-up Date</label>
+    <div className="section-form">
+      <Container>
+        <div className="form-section" style={{ boxShadow: boxShadow }}>
+          <Row style={{ alignItems: "flex-end" }}>
+            <Col lg="3" md="3" xs="12">
+              <FormGroupSelect
+                label="Pick-up Location"
+                onChange={handleChange}
+                name="pickUpLocation"
+                value={fields?.pickUpLocation}
+                defaultName="Select Location"
+              />
+            </Col>
+            <Col lg="3" md="3" xs="12">
+              <div className="form-group">
+                <label className="form-label">Pick-up Date</label>
 
-                  <DatePicker
-                    selected={selectedDate}
-                    onChange={(date) => {
-                      const inputYear = date.getFullYear().toString();
-                      // Check if the year input is longer than 4 characters
-                      if (inputYear.length > 4) {
-                        // Manipulate the year to only get the first 4 characters
-                        const year = parseInt(inputYear.substring(0, 4), 10);
-                        const newDate = new Date(date);
-                        newDate.setFullYear(year);
-                        setSelectedDate(newDate);
-                      } else {
-                        setSelectedDate(date);
-                      }
-                    }}
-                    placeholderText={" d-m-Y"}
-                    filterDate={(date) =>
-                      date.getDay() !== 6 && date.getDay() !== 0
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={(date) => {
+                    const inputYear = date.getFullYear().toString();
+                    // Check if the year input is longer than 4 characters
+                    if (inputYear.length > 4) {
+                      // Manipulate the year to only get the first 4 characters
+                      const year = parseInt(inputYear.substring(0, 4), 10);
+                      const newDate = new Date(date);
+                      newDate.setFullYear(year);
+                      setSelectedDate(newDate);
+                    } else {
+                      setSelectedDate(date);
                     }
-                    showYearDropdown
-                    scrollableYearDropdown
-                  />
-                </div>
-              </Col>
-              <Col lg="3" md="3" xs="12">
-                <div className="form-group">
-                  <label htmlFor="">Drop-off Date</label>
-                  <DatePicker
-                    selected={selectedDateof}
-                    onChange={(date) => setSelectedDateof(date)}
-                    showYearDropdown
-                    scrollableYearDropdown
-                    placeholderText={"d-m-Y"}
-                    strictParsing
-                  />
-                </div>
-              </Col>
-              <Col lg="3" md="3" xs="12">
-                <div className="findcarbtn">
-                  <Button className="btn-formPanel" color="primary">
-                    {btnText}
-                  </Button>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Container>
-      </div>
+                  }}
+                  placeholderText={" d-m-Y"}
+                  filterDate={(date) =>
+                    date.getDay() !== 6 && date.getDay() !== 0
+                  }
+                  showYearDropdown
+                  scrollableYearDropdown
+                />
+              </div>
+            </Col>
+            <Col lg="3" md="3" xs="12">
+              <div className="form-group">
+                <label htmlFor="">Drop-off Date</label>
+                <DatePicker
+                  selected={selectedDateof}
+                  onChange={(date) => setSelectedDateof(date)}
+                  showYearDropdown
+                  scrollableYearDropdown
+                  placeholderText={"d-m-Y"}
+                  strictParsing
+                />
+              </div>
+            </Col>
+            <Col lg="3" md="3" xs="12">
+              <div className="findcarbtn">
+                <Button className="btn-formPanel" color="primary">
+                  {btnText}
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Container>
     </div>
   );
 };
